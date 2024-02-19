632716f5918413fb4262261e3d7d2710eaed55f1
Supchik <eagersupchik@gmail.com>
2024-02-19 10:26:02 +0300

    Добавьте файлы проекта.

---
+++ table_vector.cpp
@@ -0,0 +1,21 @@
+﻿// table_vector.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> table(10, vector<int>(10));
+     
+    for (int i = 1; i < 10; i++)
+    {
+        for (int j = 1; j < 10; j++)
+        {
+            table[i][j] = i * j;
+            cout << table[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
